619efd07f224fd8bfe18d3213817463594bf395b
Kevinisagirl <kevin.ann.hunt@gmail.com>
2018-02-27 13:29:22 -0800

    Added a combiner for problem 2 - variance

--- mr_2_electricity_variance.py
+++ mr_2_electricity_variance.py
@@ -1,11 +1,24 @@
 from mrjob.job import MRJob
 
+# Expected to run with: python3 mr_2_electricity_variance.py Electricity.csv
+
+
 class MRElectricityVariance(MRJob):
 
     def mapper(self, _, line):
         line = line.split(',')
         state_electricity = float(line[1])
-        yield "", state_electricity
+        yield "", (state_electricity, state_electricity**2, 1)
+
+    def combiner(self, key, values):
+        total = 0
+        count = 0
+        sum_squared_x = 0
+        for v in values:
+            total += v[0]
+            count += v[2]
+            sum_squared_x += v[1]
+        yield "", (total, sum_squared_x, count)
 
     def reducer(self, key, values):
         # will be calculating variance using: (sum(x^2)/N)-mean^2
@@ -13,12 +26,13 @@ class MRElectricityVariance(MRJob):
         count = 0
         sum_squared_x = 0
         for v in values:
-            total += v
-            count += 1
-            sum_squared_x += v**2
+            total += v[0]
+            count += v[2]
+            sum_squared_x += v[1]
         mean = total/count
         variance = (sum_squared_x/count)-mean**2
         yield "variance", variance
+
 
 if __name__ == '__main__':
     MRElectricityVariance.run()
